c213dc9acf9ff3a155038818c628ee17e2ccb492
Julian Caba <julian.caba85@gmail.com>
2023-07-19 14:29:51 +0200

    Updated deactivator

--- deactivatorAT/include/config.h
+++ deactivatorAT/include/config.h
@@ -1,22 +1,46 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define _NONE 0
-#define _ACTIVATE 1
-#define _STATUS 2
+
+typedef enum {
+    AT_NONE, 
+    AT_ACTIVATE,
+    AT_STOP,
+    AT_KEEP_ALIVE_ACK,
+    AT_ERROR} tOperation;
+
+
+#define TO_DECIMAL(A) (A >= '0' && A <= '9') ? (A-'0') : (A-'A'+10)
+#define TO_UINT8(A,B) (((TO_DECIMAL(A))*16) + (TO_DECIMAL(B)))
 
 
 #define KEEP_ALIVE_TIME 2000
 
+
 #define UART_BAUDRATE 115200
 #define BUF_RX_SIZE 256
-#define BUF_TX_SIZE_KEEPALIVE 9 // pdseq 2 caracteres (legible)
+#define TX_SIZE_KEEPALIVE 9*sizeof(char) 
+#define TX_SIZE_RESPONSE_OK 12*sizeof(char) 
+#define TX_SIZE_RESPONSE_ERR 13*sizeof(char) 
+#define TX_SIZE_MAX 16
+#define TX_MUTEX_WAIT 2000
 
-#define MUTEXTX_WAIT 2000
+
+#define PULSE_SEND 0x01
+#define PULSE_END 0x00
+#define PULSE_ERR 0x02
+#define PULSE_NONE 0x00
+#define PULSE_STOP 0x01
+#define AT_ERR 0x03
+#define PULSE_RESOLUTION 100 // 100ms
 
 const char* PATTERN_KEEP_ALIVE="AT+K=%02X\r\n";
+const char* PATTERN_OK="AT+OK=%02X%02X\r\n";
+const char* PATTERN_ERR="AT+ERR=%02X%02X\r\n";
 const char* PATTERN_PREFIX_ACTIVATE="AT+A=";
-const char* PATTERN_PREFIX_STATUS="AT+S=";
-const char* PATTERN_CRLF="\r\n";
+const char* PATTERN_PREFIX_STOP="AT+S=";
+const char* PATTERN_PREFIX_KA_ACK="AT+OK";
+const char* PATTERN_CR="\r";
+const char* PATTERN_LF="\n";
 
 #endif
--- deactivatorAT/src/main.cpp
+++ deactivatorAT/src/main.cpp
@@ -14,19 +14,30 @@
 #include "freertos/semphr.h"
 
 
-QueueHandle_t sSemphrEnable;
-QueueHandle_t xMutexTX;
+QueueHandle_t sSemphrEnable = NULL;
+QueueHandle_t xMutexTX = NULL;
 
-TaskHandle_t tHandler_KeepAlive;
-TaskHandle_t tHandler_AT;
+TaskHandle_t tHandler_KeepAlive = NULL;
+TaskHandle_t tHandler_AT = NULL;
+TaskHandle_t tHandler_Pulse = NULL;
 
-static uint8_t pdseq;
+uint8_t* dataRX = NULL;
+char *msgPULSEStop = NULL;
+char *msgPULSE = NULL;//(char *)malloc(TX_SIZE_RESPONSE_OK);
+char *dataKA = NULL;//(char *)malloc(TX_SIZE_KEEPALIVE);
+char *msgTX = NULL;//(char *)malloc(TX_SIZE_RESPONSE_ERR);
+
+static volatile uint8_t pdseq;
 static uint8_t pqseq;
-static uint8_t pulse;
-static enum {stIDLE, stPLUSE_SIZE, stSEQ, stCRLF} stateAT;
-static uint8_t operation = _NONE;
+static uint8_t pdseq_ack;
+static uint8_t pqseq_ack;
+static uint16_t pulse_size_ms;
+static uint8_t pulse_pqseq;
+static enum {stIDLE, stPLUSE_SIZE, stSEQ, stEQUAL, stPDSEQ_ACK, stPQSEQ_ACK, stCR, stLF} stateAT;
+static tOperation operation = AT_NONE;
 
 void app_main(void);
+void createTasks(void);
 
 void IRAM_ATTR en_handleInterrupt(void* arg)
 {
@@ -38,7 +49,7 @@ void IRAM_ATTR en_handleInterrupt(void* arg)
 
 void transmitTX(const char *dataTX)
 {
-  xSemaphoreTake(xMutexTX, MUTEXTX_WAIT/portTICK_PERIOD_MS);
+  xSemaphoreTake(xMutexTX, TX_MUTEX_WAIT/portTICK_PERIOD_MS);
   uart_write_bytes(UART_PORT, dataTX, strlen(dataTX));
   xSemaphoreGive( xMutexTX );
 }
@@ -54,30 +65,63 @@ static void vTaskEnable( void *pvParameters )
       gpio_set_level(pinLED_off, LOW);
       //vTaskResume(tHandler_KeepAlive);
       //vTaskResume(tHandler_AT);
+      //createTasks();
     }
     else{
       gpio_set_level(pinLED_on, LOW);
       gpio_set_level(pinLED_off, HIGH);
-      //vTaskSuspend(tHandler_KeepAlive);
-      //vTaskSuspend(tHandler_AT);
+      //vTaskDelete(tHandler_KeepAlive);
+      //vTaskDelete(tHandler_AT);
+      //tHandler_AT = NULL;
+      //tHandler_KeepAlive = NULL;
     }
   }
   vTaskDelete(NULL);
 }
 
 
+void vTaskPulse(void* pvParam)
+{
+  //char *msgPULSE = (char *)malloc(TX_SIZE_RESPONSE_OK);
+  //msgPULSE = (char *)malloc(TX_SIZE_RESPONSE_OK);
+  for(;;) {
+    sprintf(msgPULSE, PATTERN_OK, pulse_pqseq, PULSE_SEND);
+    transmitTX((const char *) msgPULSE);
+    gpio_set_level(pinPulse, HIGH);  
+    vTaskDelay(pulse_size_ms/portTICK_PERIOD_MS);
+    gpio_set_level(pinPulse, LOW);
+    sprintf(msgPULSE, PATTERN_OK, pulse_pqseq, PULSE_END);
+    transmitTX((const char *) msgPULSE);
+    tHandler_Pulse = NULL;
+    vTaskDelete(NULL);
+  }
+  //free(msgPULSE);
+}
+
+void stopPulse(void)
+{
+  //char *msgPULSE2 = (char *)malloc(TX_SIZE_RESPONSE_OK);
+  //msgPULSE2 = (char *)malloc(TX_SIZE_RESPONSE_OK);
+  gpio_set_level(pinPulse, LOW);
+  sprintf(msgPULSEStop, PATTERN_OK, pqseq, PULSE_STOP);
+  transmitTX((const char *) msgPULSEStop);
+  //tHandler_Pulse = NULL;
+  vTaskDelete(tHandler_Pulse);
+  tHandler_Pulse = NULL;
+  //free(msgPULSE2);
+}
 
 
 void vTaskKeepAlive(void* pvParam)
 {
-  char *dataKA = (char *)malloc(BUF_TX_SIZE_KEEPALIVE*sizeof(char));
+  //char *dataKA = (char *)malloc(TX_SIZE_KEEPALIVE);
   for(;;) {
+    dataKA = (char *)pvPortMalloc(TX_SIZE_KEEPALIVE);
     sprintf(dataKA, PATTERN_KEEP_ALIVE, pdseq);
     transmitTX((const char *) dataKA);
-    //pdseq++;
     vTaskDelay(KEEP_ALIVE_TIME/portTICK_PERIOD_MS);
   }
-  free(dataKA);
+  //free(dataKA);
   vTaskDelete(NULL);
 }
 
@@ -85,8 +129,9 @@ void vTaskKeepAlive(void* pvParam)
 void vTaskAT(void* pvParam)
 {
   int rxBytes;
-  uint8_t* dataRX = (uint8_t*) malloc(8);
-  //uint8_t dataRX;
+  //uint8_t* dataRX = (uint8_t*) malloc(8);
+  //char *msgTX = (char *)malloc(TX_SIZE_RESPONSE_ERR);
+  //msgTX = (char *)malloc(TX_SIZE_RESPONSE_ERR);
   for(;;) {
       switch (stateAT)
       {
@@ -95,15 +140,22 @@ void vTaskAT(void* pvParam)
         if (rxBytes > 0) {
           if(strncmp((const char*)dataRX, PATTERN_PREFIX_ACTIVATE, 5) == 0){
             stateAT = stPLUSE_SIZE;
-            operation = _ACTIVATE;
+            operation = AT_ACTIVATE;
           }
           else {
-            if(strncmp((const char*)dataRX, PATTERN_PREFIX_STATUS, 5) == 0){
+            if(strncmp((const char*)dataRX, PATTERN_PREFIX_STOP, 5) == 0){
               stateAT = stSEQ;
-              operation = _STATUS;
+              operation = AT_STOP;
             }
             else{
-              stateAT = stCRLF;
+              if(strncmp((const char*)dataRX, PATTERN_PREFIX_KA_ACK, 5) == 0){
+                stateAT = stEQUAL;
+                operation = AT_KEEP_ALIVE_ACK;
+              }
+              else{
+                operation = AT_ERROR;
+                stateAT = stCR;
+              }
             }
           }
         }
@@ -112,7 +164,8 @@ void vTaskAT(void* pvParam)
       case stPLUSE_SIZE:
         rxBytes = uart_read_bytes(UART_PORT, dataRX, 2, 100 / portTICK_PERIOD_MS);
         if (rxBytes > 0) {
-          pulse = (dataRX[0]-48)*16+(dataRX[1]-48);
+          printf("[INFO] DATA %d %d\r\n", dataRX[0], dataRX[1]);
+          pulse_size_ms = (uint32_t)TO_UINT8(dataRX[0], dataRX[1]) * PULSE_RESOLUTION;
           stateAT = stSEQ;
         }
         break;
@@ -120,38 +173,98 @@ void vTaskAT(void* pvParam)
       case stSEQ:
         rxBytes = uart_read_bytes(UART_PORT, dataRX, 2, 100 / portTICK_PERIOD_MS);
         if (rxBytes > 0) {
-          pqseq = (dataRX[0]-48)*16+(dataRX[1]-48);
-          stateAT = stCRLF;
+          pqseq = TO_UINT8(dataRX[0], dataRX[1]);
+          if (operation == AT_ACTIVATE && tHandler_Pulse == NULL)
+            pulse_pqseq = pqseq;
+          stateAT = stCR;
         }
         break;
 
-      case stCRLF:
+      case stEQUAL:
+        rxBytes = uart_read_bytes(UART_PORT, dataRX, 1, 100 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {          
+          if(strncmp((const char*)dataRX, "=", 1) == 0){
+            stateAT = stPDSEQ_ACK;
+          }
+          else{
+            operation = AT_ERROR;
+            stateAT = stCR;
+          }
+        }
+        break;
+
+      case stPDSEQ_ACK:
         rxBytes = uart_read_bytes(UART_PORT, dataRX, 2, 100 / portTICK_PERIOD_MS);
         if (rxBytes > 0) {
-          if(strncmp((const char*)dataRX, PATTERN_CRLF, 2) == 0){
+          pdseq_ack = TO_UINT8(dataRX[0], dataRX[1]);
+          stateAT = stPQSEQ_ACK;
+        }
+        break;
+
+      case stPQSEQ_ACK:
+        rxBytes = uart_read_bytes(UART_PORT, dataRX, 2, 100 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+          pqseq_ack = TO_UINT8(dataRX[0], dataRX[1]);
+          stateAT = stCR;
+        }
+        break;
+
+      case stCR:
+        rxBytes = uart_read_bytes(UART_PORT, dataRX, 1, 100 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+          if(strncmp((const char*)dataRX, PATTERN_CR, 1) == 0){
+            stateAT = stLF;
+          }
+          else{
+            operation = AT_ERROR;
+          }
+        }
+        break;
+
+      case stLF:
+        rxBytes = uart_read_bytes(UART_PORT, dataRX, 1, 100 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+          if(strncmp((const char*)dataRX, PATTERN_LF, 1) == 0){
             pdseq++;
             stateAT = stIDLE;
             switch (operation)
             {
-            case _NONE:
+            case AT_ERROR:
               printf("[INFO] ERROR\r\n");
-              transmitTX("ERROR\r\n");
+              sprintf(msgTX, PATTERN_ERR, pqseq, AT_ERR);
+              transmitTX((const char *) msgTX);
               break;
 
-            case _ACTIVATE:
-              printf("[INFO] ACTIVATE %d %d\r\n", pulse, pqseq);
-              transmitTX("ACTIVATE\r\n");
+            case AT_ACTIVATE:
+              printf("[INFO] ACTIVATE %d %d\r\n", pulse_size_ms, pqseq);
+              if (tHandler_Pulse == NULL)
+                xTaskCreatePinnedToCore(vTaskPulse, "Task Pulse", 2000, NULL, 1, &tHandler_Pulse, 1);
+              else{
+                sprintf(msgTX, PATTERN_ERR, pqseq, PULSE_ERR);
+                transmitTX((const char *) msgTX);
+              }
               break;
 
-            case _STATUS:
-              printf("[INFO] STATUS %d\r\n", pqseq);
-              transmitTX("STATUS\r\n");
+            case AT_STOP:
+              printf("[INFO] STOP %d\r\n", pqseq);
+              if (tHandler_Pulse == NULL){
+                sprintf(msgTX, PATTERN_OK, pqseq, PULSE_NONE);
+                transmitTX((const char *) msgTX);
+              }
+              else{
+                stopPulse();
+              }
+              break;
+
+            case AT_KEEP_ALIVE_ACK:
+              // ToDo: Further versions - Check ACK
+              printf("[INFO] KEEP ALIVE ACK %d %d\r\n", pdseq_ack, pqseq_ack);
               break;
             
             default:
               break;
             }
-            operation = _NONE;
+            operation = AT_NONE;
           }
 
       
@@ -160,7 +273,8 @@ void vTaskAT(void* pvParam)
       }
     }    
   }
-  free(dataRX);
+  //free(dataRX);
+  //free(msgTX);
   vTaskDelete(NULL);
 }
 
@@ -177,7 +291,7 @@ void setup()
   gpio_config_t io_conf = {};
   io_conf.intr_type = GPIO_INTR_DISABLE;
   io_conf.mode = GPIO_MODE_OUTPUT;
-  io_conf.pin_bit_mask = ((1ULL<<pinLED_off) | (1ULL<<pinLED_on) | (1ULL<<pinLED_USB_ok) | (1ULL<<pinLED_USB_err));
+  io_conf.pin_bit_mask = ((1ULL<<pinLED_off) | (1ULL<<pinLED_on) | (1ULL<<pinLED_USB_ok) | (1ULL<<pinLED_USB_err) | (1ULL<<pinPulse));
   io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
   io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
   gpio_config(&io_conf);
@@ -202,8 +316,16 @@ void setup()
   gpio_set_level(pinLED_off, LOW);
   gpio_set_level(pinLED_USB_ok, LOW);
   gpio_set_level(pinLED_USB_err, LOW);
+  gpio_set_level(pinPulse, LOW);
 
   pdseq = 0;
+
+  // FreeRTOS-ESP32 uses heap_1 so free is not allowed
+  dataRX = (uint8_t*) malloc(8);
+  msgPULSEStop = (char *)malloc(TX_SIZE_RESPONSE_OK);
+  msgPULSE = (char *)malloc(TX_SIZE_RESPONSE_OK);
+  dataKA = (char *)malloc(TX_SIZE_KEEPALIVE);
+  msgTX = (char *)malloc(TX_SIZE_RESPONSE_ERR);
 
   app_main();
 }
@@ -227,11 +349,16 @@ void app_main(void)
     printf("[FAIL] TX Mutex has not been created\n");
     return;
   }
-   
+
   xTaskCreatePinnedToCore(vTaskEnable, "Task Enable", 2500, NULL, 10, NULL, 0);
+  createTasks();
+
+}
+
+void createTasks(void)
+{
   xTaskCreatePinnedToCore(vTaskKeepAlive, "Task KA", 5000, NULL, 2, &tHandler_KeepAlive, 0);
   xTaskCreatePinnedToCore(vTaskAT, "Task AT", 5000, NULL, 1, &tHandler_AT, 0);
-
 }
 
 void loop() {
